83ee1ea5605c73fe99d3591794d63842eae5105a
33bca <fabian.leutenegger@bluewin.ch>
2021-02-20 13:45:13 +0530

    platina: Adapt ANT+ for CAF
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -136,10 +136,8 @@ $(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-syste
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    antradio_app \
+    libantradio
 
 # ATRACE_HAL
 PRODUCT_PACKAGES += \
